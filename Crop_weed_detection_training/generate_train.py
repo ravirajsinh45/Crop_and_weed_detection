27b86038c1923676ccaac57100866340b33869d4
Ravirajsinh Dabhi <ravirajsinhdabhi86@gmail.com>
2020-05-13 12:57:46 +0530

    added two seperate diractory, one for training and second for running model on images

---
+++ Crop_weed_detection_training/generate_train.py
@@ -0,0 +1,14 @@
+import os
+
+image_files = []
+os.chdir(os.path.join("data", "data"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpeg"):
+        image_files.append("data/data/" + filename)
+os.chdir("..")
+with open("train.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
